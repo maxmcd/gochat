d6b76b4dd40d6d321f794dde5eb5cded94db1db0
maxmcd <max.t.mcdonnell@gmail.com>
2015-07-26 22:28:13 -0400

    coffee script, js based views

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var coffee = require('gulp-coffee');
 
 gulp.task('sass', function() {
   	return sass('app/sass/style.sass')
@@ -9,14 +10,25 @@ gulp.task('sass', function() {
     	.pipe(reload({ stream:true }));
 });
 
+gulp.task('coffee', function() {
+  gulp.src('app/coffee/*.coffee')
+    .pipe(coffee({bare: true})).on('error', console.log)
+    .pipe(gulp.dest('app/js'))
+    .pipe(reload({ stream:true }));
+});
+
 // watch Sass files for changes, run the Sass preprocessor with the 'sass' task and reload
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['sass', 'coffee'], function() {
   	browserSync({
     	server: {
       		baseDir: 'app'
     	}
   	});
 
-	gulp.watch(['*.html', 'app/script/**/*.js'], {cwd: 'app'}, reload);
+	gulp.watch(['*.html'], {cwd: 'app'}, reload);
 	gulp.watch('app/sass/*.sass', ['sass']);
+  gulp.watch('app/coffee/*.coffee', ['coffee']);
 });
+
+
+
