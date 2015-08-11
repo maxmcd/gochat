22d8b6b7b35a4dea26a2fb29951453b974aa2947
maxmcd <max.t.mcdonnell@gmail.com>
2015-08-11 00:09:03 -0400

    Update 2015-08-11T04:09:01.517Z

--- js/app.js
+++ js/app.js
@@ -1,196 +1,336 @@
-var bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+var Input = React.createClass({displayName: "Input",
+	componentDidMount: function() {
+		$('.question').focus()
+	},
+	handleSubmit: function(e) {
+		e.preventDefault()
+		node = React.findDOMNode(
+			this.refs.whereTypingHappens
+		)
+		this.props.handleSubmit(
+			node.value
+		)
+		node.value = ""
+	},
+	render: function() {
+		var avatarStyle = {
+			backgroundColor: this.props.color
+		}
+		return (
+			React.createElement("div", null, 
+				React.createElement("div", {className: "input"}, 
+					React.createElement("form", {action: "#", onSubmit: this.handleSubmit}, 
+						React.createElement("div", {className: "avatar", 
+							style: avatarStyle}), 
+						React.createElement("input", {
+							ref: "whereTypingHappens", 
+							type: "text", 
+							className: "question", 
+							placeholder: this.props.placeholder, autofocus: true}), 
+						React.createElement("input", {type: "submit", value: "submit"})
+					)
+				)
+			)
+		)
+	}	
+})
 
-window.CH = {};
+var Message = React.createClass({displayName: "Message",
+	render: function() {
+		var style = {
+			backgroundColor: this.props.message.color
+		}
+		return (
+			React.createElement("div", {className: "message", key: this.props.index}, 
+				React.createElement("div", {className: "avatar", style: style}), 
+				this.props.message.body
+			)
+		)
+	}
+})
 
-window.questions = [
-  {
-    "question": "Why is the sky blue?",
-    "color": "AliceBlue"
-  }, {
-    "question": "How will I get down from here?",
-    "color": "AntiqueWhite"
-  }, {
-    "question": "What is up with these grapes?",
-    "color": "Aqua"
-  }
-];
+var Question = React.createClass({displayName: "Question",
+	componentDidMount: function() {
+		this.props.getData()
+		this.props.poll()
+	},
+	createMessage: function(message, index) {
+		return (
+			React.createElement(Message, {
+				message: message, 
+				index: index}
+			)
+		)
+	},
+	render: function() {
+		var qAvatarStyle = {
+			backgroundColor: this.props.chat.color
+		}
+		return (
+			React.createElement("div", null, 
+				React.createElement("div", {className: "blob"}, 
+					React.createElement("div", {className: "avatar", style: qAvatarStyle}), 
+					this.props.chat.question
+				), 
+				this.props.chat.messages.map(this.createMessage), 
+				React.createElement(Input, {
+					handleSubmit: this.props.handleSubmit, 
+					color: this.props.color}
+				)
+			)
+		)
+	}
+})
 
-window.chat = {
-  "question": {
-    "question": "Why is the sky blue?",
-    "color": "AliceBlue"
-  },
-  "answers": [
-    {
-      "message": "Why is the sky blue?",
-      "color": "AliceBlue"
-    }, {
-      "message": "How will I get down from here?",
-      "color": "AntiqueWhite"
-    }, {
-      "message": "What is up with these grapes?",
-      "color": "Aqua"
-    }
-  ]
-};
+var QuestionLink = React.createClass({displayName: "QuestionLink",
+	render: function() {
+		var key = "#" + this.props.question.key
+		var style = {
+			backgroundColor: this.props.question.color
+		}
+		return (
+			React.createElement("a", {href: key, key: this.props.index, className: "blob"}, 
+				React.createElement("div", {className: "avatar", style: style}), 
+				this.props.question.question
+			)            			
+		)
+	}
+})
 
-CH.Tools = (function() {
-  function Tools() {}
+var Home = React.createClass({displayName: "Home",
+	componentDidMount: function() {
+		this.props.getData()
+	},
+	createQuestion: function(question, index) {
+		return (
+			React.createElement(QuestionLink, {question: question, index: index})
+		)
+	},
+	render: function() {
+		return (
+			React.createElement("div", null, 
+				React.createElement(Input, {
+					placeholder: "ask your question...", 
+					handleSubmit: this.props.handleSubmit, 
+					color: this.props.color}
+				), 
+				this.props.questions.map(this.createQuestion)
+			)
+		)
+	}
+})
 
-  Tools.generateColor = function() {
-    var color, colors;
-    colors = ['AliceBlue', 'AntiqueWhite', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque', 'Black', 'BlanchedAlmond', 'Blue', 'BlueViolet', 'Brown', 'BurlyWood', 'CadetBlue', 'Chartreuse', 'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan', 'DarkBlue', 'DarkCyan', 'DarkGoldenRod', 'DarkGray', 'DarkGreen', 'DarkKhaki', 'DarkMagenta', 'DarkOliveGreen', 'DarkOrange', 'DarkOrchid', 'DarkRed', 'DarkSalmon', 'DarkSeaGreen', 'DarkSlateBlue', 'DarkSlateGray', 'DarkTurquoise', 'DarkViolet', 'DeepPink', 'DeepSkyBlue', 'DimGray', 'DodgerBlue', 'FireBrick', 'FloralWhite', 'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite', 'Gold', 'GoldenRod', 'Gray', 'Green', 'GreenYellow', 'HoneyDew', 'HotPink', 'IndianRed', 'Indigo', 'Ivory', 'Khaki', 'Lavender', 'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue', 'LightCoral', 'LightCyan', 'LightGoldenRodYellow', 'LightGray', 'LightGreen', 'LightPink', 'LightSalmon', 'LightSeaGreen', 'LightSkyBlue', 'LightSlateGray', 'LightSteelBlue', 'LightYellow', 'Lime', 'LimeGreen', 'Linen', 'Magenta', 'Maroon', 'MediumAquaMarine', 'MediumBlue', 'MediumOrchid', 'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 'MediumSpringGreen', 'MediumTurquoise', 'MediumVioletRed', 'MidnightBlue', 'MintCream', 'MistyRose', 'Moccasin', 'NavajoWhite', 'Navy', 'OldLace', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed', 'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 'PaleVioletRed', 'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 'PowderBlue', 'Purple', 'RebeccaPurple', 'Red', 'RosyBrown', 'RoyalBlue', 'SaddleBrown', 'Salmon', 'SandyBrown', 'SeaGreen', 'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue', 'SlateGray', 'Snow', 'SpringGreen', 'SteelBlue', 'Tan', 'Teal', 'Thistle', 'Tomato', 'Turquoise', 'Violet', 'Wheat', 'White', 'WhiteSmoke', 'Yellow', 'YellowGreen'];
-    color = colors[Math.floor(Math.random() * colors.length)];
-    return color;
-  };
-
-  Tools.escape = function(s) {
-    return ('' + s).replace(/&/g, '&').replace(/</g, '<').replace(/>/g, '>').replace(/"/g, '"').replace(/'/g, "'").replace(/\//g, '/');
-  };
-
-  return Tools;
-
-})();
-
-CH.App = (function() {
-  function App(options) {
-    this.options = options;
-    this.runRouter();
-    this.setRandomAvatars();
-    window.onpopstate = history.onpushstate = this.runRouter;
-  }
-
-  App.prototype.setRandomAvatars = function() {
-    var colors;
-    colors = this.colors;
-    return $('.avatar').each((function(_this) {
-      return function(index, el) {
-        var color;
-        if (!$(el).hasClass('logo')) {
-          color = CH.Tools.generateColor();
-          return $(el).css({
-            'background-color': color
-          });
-        }
-      };
-    })(this));
-  };
-
-  App.prototype.runRouter = function() {
-    var location;
-    location = document.location.hash;
-    $('.canvas').html('');
-    $('.header').removeClass('back');
-    if (location === "") {
-      return new CH.Questions(window.questions);
-    } else {
-      return new CH.Chat(window.chat);
-    }
-  };
-
-  return App;
-
-})();
-
-CH.Questions = (function() {
-  function Questions(questions) {
-    this.submit = bind(this.submit, this);
-    this.createQuestion = bind(this.createQuestion, this);
-    this.appendQuestions = bind(this.appendQuestions, this);
-    this.initialRender = bind(this.initialRender, this);
-    this.initialRender();
-    $('form').submit(this.submit);
-    this.input = $('input[type=text]');
-    this.input.focus();
-  }
-
-  Questions.prototype.initialRender = function() {
-    var i, input, len, x;
-    input = "<div class=\"input\">\n    <form action=\"#\">\n        <div class=\"avatar\"></div>\n        <input type=\"text\" class=\"question\" placeholder=\"ask your question...\" autofocus>\n        <input type=\"submit\" value=\"submit\">             \n    </form>\n</div>";
-    this.content = input;
-    for (i = 0, len = questions.length; i < len; i++) {
-      x = questions[i];
-      this.appendQuestions(x);
-    }
-    return $('.canvas').html(this.content);
-  };
-
-  Questions.prototype.appendQuestions = function(obj) {
-    return this.content += this.createQuestion(obj);
-  };
-
-  Questions.prototype.createQuestion = function(question) {
-    return "<a href=\"#foo\" class=\"blob\">\n    <div class=\"avatar\"></div>\n    " + (CH.Tools.escape(question.question)) + "\n</a>            ";
-  };
-
-  Questions.prototype.submit = function(e) {
-    var q;
-    e.preventDefault();
-    q = this.input.val();
-    this.input.val('');
-    return $('.input').after(this.createQuestion({
-      question: q
-    }));
-  };
-
-  return Questions;
-
-})();
-
-CH.Chat = (function() {
-  function Chat(chat) {
-    this.submit = bind(this.submit, this);
-    this.createAnswer = bind(this.createAnswer, this);
-    this.createQuestion = bind(this.createQuestion, this);
-    this.appendAnswers = bind(this.appendAnswers, this);
-    this.initialRender = bind(this.initialRender, this);
-    this.initialRender();
-    $('form').submit(this.submit);
-    this.input = $('input[type=text]');
-    this.input.focus();
-  }
-
-  Chat.prototype.initialRender = function() {
-    var i, input, len, ref, x;
-    this.content = "";
-    input = "<div class=\"input\">\n    <form action=\"#\">\n        <div class=\"avatar\"></div>\n        <input type=\"text\" class=\"question\" placeholder=\"ask your question...\" autofocus>\n        <input type=\"submit\" value=\"submit\">                \n    </form>\n</div>";
-    $('.header').addClass('back');
-    this.content += this.createQuestion(chat.question);
-    ref = chat.answers;
-    for (i = 0, len = ref.length; i < len; i++) {
-      x = ref[i];
-      this.appendAnswers(x);
-    }
-    this.content += input;
-    return $('.canvas').html(this.content);
-  };
-
-  Chat.prototype.appendAnswers = function(obj) {
-    return this.content += this.createAnswer(obj);
-  };
-
-  Chat.prototype.createQuestion = function(question) {
-    return "<a href=\"#foo\" class=\"blob\">\n    <div class=\"avatar\"></div>\n    " + (CH.Tools.escape(question.question)) + "\n</a>            ";
-  };
-
-  Chat.prototype.createAnswer = function(obj) {
-    return "<div class=\"message\">\n	<div class=\"avatar\"></div>\n	" + obj.message + "\n</div>";
-  };
-
-  Chat.prototype.submit = function(e) {
-    var q;
-    e.preventDefault();
-    q = this.input.val();
-    this.input.val('');
-    return $('.input').before(this.createAnswer({
-      message: q
-    }));
-  };
-
-  return Chat;
-
-})();
+var Canvas = React.createClass({displayName: "Canvas",
+	retrieveColor: function() {
+		var color = Tools.getCookie('color')
+		if (!color) {
+			color = Tools.generateColor()
+			Tools.setCookie('color', color)
+		}
+		return color
+	},
+	getLocation: function() {
+		return document.location.hash.slice(1)
+	},
+	attachEventHandlers: function() {
+		// history state change
+		window.onpopstate = history.onpushstate = function() {
+			this.setState({location: this.getLocation()})
+		}.bind(this)
+	},
+	getInitialState: function() {
+		this.attachEventHandlers()
+		return {
+			color: this.retrieveColor(),
+			chat: {
+				created_at: 0,
+				question: null,
+				messages: []
+			},
+			questions: [],
+			location: this.getLocation()
+		}
+	},
+	getChat: function() {
+		$.ajax({
+			url: "http://localhost:8001/chat/" + this.state.location + "/",
+			dataType: 'json',
+			success: function(chat) {
+				if (chat.messages == null){
+					chat.messages = []
+				}
+				this.setState({chat: chat})
+			}.bind(this),
+			error: function(data) {
+				console.log('404')
+				this.setState({location: ""})
+			} 
+		})
+	},
+	poll: function(location) {
+		var latest = 1
+		var messages = this.state.chat.messages
+		if (messages.length > 0) {
+			latest = messages[messages.length-1].created_at
+		}
+		$.ajax({
+			url: "http://localhost:8001/listen/"+this.state.location+"/",
+			dataType: "json",
+			data: {
+				latest: latest
+			},
+			success: function(messages) {
+				if (location == this.state.location) {
+					this.state.chat.messages = this.state.chat.messages.concat(messages)
+					this.setState({chat: this.state.chat})
+					// hmmmmmmmm
+					// racey?					
+				}
+				if (this.state.location !== "") {
+					window.setTimeout(function() {
+						this.poll(this.state.location)
+					}.bind(this), 300)
+				}
+			}.bind(this),
+			error: function() {
+				window.setTimeout(function() {
+					this.poll(this.state.location)
+				}.bind(this), 1000)
+			}.bind(this)
+		})
+	},
+	writeQuestion: function(question) {
+		$.ajax({
+			url: 'http://localhost:8001/chats/',
+			type: 'post',
+			data: {
+				question: question,
+				color: this.state.color
+			},
+			success: function(data) {
+				window.location = "#" + data
+			},
+			error: function(data) {
+				console.log(data)
+			}
+		})
+	},
+	writeMessage: function(message) {
+		if (this.state.chat.question !== null) {
+			$.ajax({
+				url: "http://localhost:8001/chat/" + this.state.location + "/",
+				type: 'post',
+				data: {
+					color: this.state.color,
+					message: message
+				},
+				success: function(data) {
+					console.log(data)
+				},
+				error: function(data) {
+					console.log(data)
+				}
+			})
+		}
+	},
+	getQuestions: function() {
+		// clear chat when you go back home
+		// so that it doesn't render
+		// when a different chat is selected
+		this.setState({chat: {
+			created_at: 0,
+			question: null,
+			messages: []
+		}})
+		$.ajax({
+			url: 'http://localhost:8001/chats/',
+			dataType: 'json',
+			success: function(questions) {
+				this.setState({questions: questions})
+			}.bind(this)
+		})
+	},
+	content: function() {
+		if (this.state.location == "") {
+			return (
+				React.createElement(Home, {
+					questions: this.state.questions, 
+					getData: this.getQuestions, 
+					handleSubmit: this.writeQuestion, 
+					color: this.state.color}
+				)
+			)
+		} else {
+			return (
+				React.createElement(Question, {
+					poll: this.poll, 
+					chat: this.state.chat, 
+					getData: this.getChat, 
+					handleSubmit: this.writeMessage, 
+					questions: this.state.questions, 
+					color: this.state.color}
+				)
+			)
+		}		
+	},
+	icon: function() {
+		if (this.state.location == "") {
+			return (React.createElement("div", {className: "avatar logo"}))
+		} else {
+			return (React.createElement("div", {className: "back-arrow"}))
+		}
+	},
+	render: function() {
+		console.log(this.state)
+		return (
+			React.createElement("div", null, 
+		        React.createElement("a", {href: "#", className: "header blob"}, 		            
+		        	this.icon(), 
+		            "welcome to chat"
+		        ), 
+		        this.content()
+		    )
+	    )
+	}
+});
 
 $(function() {
-  return new CH.App();
-});
+	React.render(React.createElement(Canvas, null), document.getElementById('canvas'))	
+})
+
+Tools = {
+    generateColor: function() {
+        var color, colors;
+        colors = ['AliceBlue', 'AntiqueWhite', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque', 'Black', 'BlanchedAlmond', 'Blue', 'BlueViolet', 'Brown', 'BurlyWood', 'CadetBlue', 'Chartreuse', 'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan', 'DarkBlue', 'DarkCyan', 'DarkGoldenRod', 'DarkGray', 'DarkGreen', 'DarkKhaki', 'DarkMagenta', 'DarkOliveGreen', 'DarkOrange', 'DarkOrchid', 'DarkRed', 'DarkSalmon', 'DarkSeaGreen', 'DarkSlateBlue', 'DarkSlateGray', 'DarkTurquoise', 'DarkViolet', 'DeepPink', 'DeepSkyBlue', 'DimGray', 'DodgerBlue', 'FireBrick', 'FloralWhite', 'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite', 'Gold', 'GoldenRod', 'Gray', 'Green', 'GreenYellow', 'HoneyDew', 'HotPink', 'IndianRed', 'Indigo', 'Ivory', 'Khaki', 'Lavender', 'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue', 'LightCoral', 'LightCyan', 'LightGoldenRodYellow', 'LightGray', 'LightGreen', 'LightPink', 'LightSalmon', 'LightSeaGreen', 'LightSkyBlue', 'LightSlateGray', 'LightSteelBlue', 'LightYellow', 'Lime', 'LimeGreen', 'Linen', 'Magenta', 'Maroon', 'MediumAquaMarine', 'MediumBlue', 'MediumOrchid', 'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 'MediumSpringGreen', 'MediumTurquoise', 'MediumVioletRed', 'MidnightBlue', 'MintCream', 'MistyRose', 'Moccasin', 'NavajoWhite', 'Navy', 'OldLace', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed', 'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 'PaleVioletRed', 'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 'PowderBlue', 'Purple', 'RebeccaPurple', 'Red', 'RosyBrown', 'RoyalBlue', 'SaddleBrown', 'Salmon', 'SandyBrown', 'SeaGreen', 'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue', 'SlateGray', 'Snow', 'SpringGreen', 'SteelBlue', 'Tan', 'Teal', 'Thistle', 'Tomato', 'Turquoise', 'Violet', 'Wheat', 'White', 'WhiteSmoke', 'Yellow', 'YellowGreen'];
+        color = colors[Math.floor(Math.random() * colors.length)];
+        return color;
+    },
+    escape: function(s) {
+        return ('' + s).replace(/&/g, '&').replace(/</g, '<').replace(/>/g, '>').replace(/"/g, '"').replace(/'/g, "'").replace(/\//g, '/');
+    },
+    setCookie: function(name, value) {
+        var d, days, expires;
+        d = new Date();
+        days = 3000;
+        d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "expires=" + d.toUTCString();
+        return document.cookie = name + "=" + value + "; " + expires;
+    },
+    getCookie: function(cname) {
+        var c, ca, i, name;
+        name = cname + '=';
+        ca = document.cookie.split(';');
+        i = 0;
+        while (i < ca.length) {
+            c = ca[i];
+            while (c.charAt(0) === ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) === 0) {
+                return c.substring(name.length, c.length);
+            }
+            i++;
+        }
+        return null;
+    }
+}
