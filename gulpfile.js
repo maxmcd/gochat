48a15ac9421cf9097cee9950b7015dae996c6202
maxmcd <max.t.mcdonnell@gmail.com>
2015-08-11 00:08:09 -0400

    browsersync out, livereload in

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,23 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    livereload = require('gulp-livereload'),
     concat = require('gulp-concat'),
     sass = require('gulp-ruby-sass'),
-    browserSync = require('browser-sync'),
-    reload = browserSync.reload,
     coffee = require('gulp-coffee'),
     ghPages = require('gulp-gh-pages'),
-    react = require('gulp-react');
- 
+    react = require('gulp-react'),
+    http = require('http'),
+    st = require('st')
+
 gulp.task('jsx', function () {
     return gulp.src('app/jsx/*.jsx')
         .pipe(react({harmony: true}))
-        .on('error', console.log.bind(console))
+        .on('error', function(e){
+            console.log(e);
+            this.emit('end');
+        })
         .pipe(concat('app.js'))
         .pipe(gulp.dest('app/dist/js'))
-        .pipe(reload({ stream:true }));
+        .pipe(livereload());
 });
  
 gulp.task('deploy', function() {
@@ -24,14 +28,14 @@ gulp.task('deploy', function() {
 gulp.task('sass', function() {
   	return sass('app/sass/style.sass')
     	.pipe(gulp.dest('app/dist/css'))
-    	.pipe(reload({ stream:true }));
+    	.pipe(livereload());
 });
 
 gulp.task('vendor', function() {
     gulp.src('app/js/*.js')
         .pipe(concat('vendor.js'))
         .pipe(gulp.dest('app/dist/js'))
-        .pipe(reload({ stream:true }));
+        .pipe(livereload());
 })
 
 gulp.task('move-files', function() {
@@ -39,19 +43,23 @@ gulp.task('move-files', function() {
         .pipe(gulp.dest('app/dist'));
 })
 
-// watch Sass files for changes, run the Sass preprocessor with the 'sass' task and reload
-gulp.task('serve', ['move-files','sass', 'jsx', 'vendor'], function() {
-  	browserSync({
-    	server: {
-      		baseDir: 'app/dist'
-    	}
-  	});
 
-	gulp.watch(['app/*'], ['move-files', reload]);
+// watch Sass files for changes, run the Sass preprocessor with the 'sass' task and reload
+gulp.task('serve', ['server', 'move-files','sass', 'jsx', 'vendor'], function() {
+    livereload.listen({
+        basePath: "app/dist",
+        port: '3000'
+    });
+	gulp.watch(['app/*'], ['move-files', livereload()]);
 	gulp.watch('app/sass/*.sass', ['sass']);
     gulp.watch('app/jsx/*', ['jsx']);
     gulp.watch('app/js/*.js', ['vendor']);
 });
 
+gulp.task('server', function(done) {
+  http.createServer(
+    st({ path:'app/dist', index: 'index.html', cache: false })
+  ).listen(8080, done);
+});
 
 
